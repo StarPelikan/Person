44526d8f8a887bdc4be1b4d006b7bc8011e99996
StarPelikan <cccp-89@bk.ru>
2022-11-24 11:19:15 +0300

    eraser

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
         people.add(new Person("Евгений","Малкин", 36));
-        people.add(new Person("Александр","Мостовой", 54));
+        people.add(new Person("Александр","Пенкин", 14));
         people.add(new Person("Николвай","Хабибулин", 49));
-        people.add(new Person("Екатерина ","Гамова", 42));
+        people.add(new Person("Екатерина ","Гарсиева", 12));
         people.add(new Person("Павел","Буре", 51));
         people.add(new Person("Ганс", "Бобруй Якутский", 29));
         System.out.println(people);
         
         Collections.sort(people,new  PetsonComparator());
         System.out.println(people);
+        Collections.sort(people, new PetsonComparator());
+
+        Predicate<Person> years18= (person)->(18>person.getAge()) ? true:false;
+        people.removeIf(years18);
+        System.out.println("В список не вошли кому нет 18 лет: " + people);
+
+
+
     }
 }
