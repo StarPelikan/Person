26bd5f91192c56a065bc1ab1982d5d5c81369eb3
StarPelikan <cccp-89@bk.ru>
2022-11-24 11:23:58 +0300

    lambda

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Евгений","Малкин", 36));
+        people.add(new Person("Евгений", "Малкин", 36));
         people.add(new Person("Ганс", "Бобруй Якутский", 29));
-        people.add(new Person("Александр","Мостовой", 54));
-        people.add(new Person("Николвай","Хабибулин", 49));
-        people.add(new Person("Екатерина ","Гамова", 42));
-        people.add(new Person("Павел","Буре", 51));
+        people.add(new Person("Александр", "Мостовой", 54));
+        people.add(new Person("Николвай", "Хабибулин", 49));
+        people.add(new Person("Екатерина ", "Гамова", 42));
+        people.add(new Person("Павел", "Буре", 51));
 
         System.out.println(people);
-        Comparator<Person> comparator = (Person o1, Person o2)-> {
+        Comparator<Person> comparator = (Person o1, Person o2) -> {
             int text1 = o1.getSurname().split(" ").length;
             int text2 = o2.getSurname().split(" ").length;
             if (text1 != text2) {
@@ -24,7 +24,7 @@ public class Main {
             }
         };
 
-        Collections.sort(people,comparator);
+        Collections.sort(people, comparator);
         System.out.println(people);
     }
 }
